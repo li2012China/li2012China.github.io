5d26b91bb8bb7b21f886fe7b8c3704731574e00d
li2012 <li2012China@outlook.com>
2024-11-27 21:04:50 +0800

    Add files via upload

---
+++ li2012China.js
@@ -0,0 +1,46 @@
+class OpenUrlExtension {
+    constructor(runtime) {
+        this.runtime = runtime;
+    }
+
+    getInfo() {
+        return {
+            id: "openUrl",
+            name: "打开标签页",
+            blocks: [
+                {
+                    opcode: "open",
+                    blockType: Scratch.BlockType.COMMAND,
+                    text: "打开网址 [URL]",
+                    arguments: {
+                        URL: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: "https://www.example.com"
+                        }
+                    }
+                }
+            ]
+        };
+    }
+
+    open(args) {
+        const url = args.URL;
+        window.open(url, '_blank');
+    }
+
+    log(args) {
+        console.log(args.MESSAGE);
+    }
+
+    getOpenAppCommand(app) {
+        const commands = {
+            Win: `cmd://run ${app}`,
+            Mac: `open ${app}`,
+            Linux: `xdg-open ${app}`
+        };
+        const platform = navigator.platform.toLowerCase();
+        return commands[platform] || null;
+    }
+}
+
+Scratch.extensions.register(new OpenUrlExtension());
